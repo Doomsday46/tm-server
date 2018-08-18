6c23ad3f7d87b2de3d63880bccf7ddf9de7a3bc6
Vitaliy <Gallifrey46@gmail.com>
2018-08-18 17:00:16 +0300

    init project

---
+++ src/main/java/com/doomsday/tmserver/TmserverApplication.java
@@ -0,0 +1,12 @@
+package com.doomsday.tmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmserverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/doomsday/tmserver/TmserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.doomsday.tmserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TmserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
